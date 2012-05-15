2ef085daa83ce7bb9eb4753291d405d2aad0fa1b
Ronald Kaiser <raios.catodicos@gmail.com>
2012-05-14 22:37:14 -0300

    adding globals

---
+++ globals.js
@@ -0,0 +1,51 @@
+/*
+    File: globals.js
+    Description: nodejs global objects
+*/
+
+//global: the global namespace object
+console.log(global)
+
+//process
+console.log(process)
+
+//console
+console.log(console)
+
+//Buffer: handles binary data
+console.log(Buffer)
+
+//require: requires modules
+console.log(require)
+
+//require.resolve(): returns the resolved filename instead of loading
+console.log(require.resolve)
+
+//require.cache: loaded modules with require() are saved here
+console.log(require.cache)
+
+//__filename: current filename being executed
+//__filename is local to each module
+console.log(__filename)
+
+//__dirname: current dirname
+console.log(__dirname)
+
+//module: reference to current module
+console.log(module)
+
+//exports: object shared between all instances of the current module
+console.log(exports)
+
+//setTimeout: schedules a function
+t = setTimeout(function() {console.log('timed out!')}, 2000)
+
+//clearTimeout: stops setTimeout
+clearTimeout(t)
+
+//setInterval: schedules a function periodically
+f = setInterval(function() {console.log('timed out!')}, 1000)
+
+//clearInterval: stops setInterval
+clearInterval(f)
+
